2e82c99173e0b750a69a4a25728904a89db078f9
GuilhermeSRei <139374621+GuilhermeSRei@users.noreply.github.com>
2023-09-08 16:07:11 -0300

    projeto mochila armazenando dados
    Aprendendo a armazenar dados no localstorage

--- js/main.js
+++ js/main.js
@@ -2,41 +2,85 @@ const form = document.querySelector('#novoItem')
 
 const lista = document.querySelector('#lista')
 
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-form.addEventListener('submit', (evento) => {
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+});
+
+form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+    console.log(existe)
+
+    const itemAtual = {
+        'nome': nome.value,
+        'quantidade': quantidade.value
+    }
+
+    if(existe) {
+        itemAtual.id = existe.id
+
+        atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    } else {
+        itemAtual.id = itens[itens.lenght - 1] ? itens([itens.lenght - 1]).id - 1 : 0
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 
     nome.value = ""
     quantidade.value = ""
 
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
+    const novoItem = document.createElement("li")
 
-    const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+
+    novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta(item.id))
 
     lista.appendChild(novoItem)
+}
 
-    const itemAtual = {
-        'nome': nome,
-         'quantidade': quantidade
-    }
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
 
-    itens.push(itemAtual)
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement('button')
+    elementoBotao.innerText = 'X'
 
-    localStorage.setItem("item", JSON.stringify(itemAtual))
+    elementoBotao.addEventListener('click', function() {
+        deletaElemento(this.parentNode, id)
+    })
 
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id == id), 1)
+
+    console.log(itens)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
